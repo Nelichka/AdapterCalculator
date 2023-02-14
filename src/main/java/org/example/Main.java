5c4986ecf9e1528ae88423c703bb3ec462740c29
Andreeva Nina <nandreewaa@gmail.com>
2023-02-14 14:49:52 +0300

    Correctirovka

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Ints intsCalc = new IntsCalculator();
-        System.out.println(intsCalc.sum(3, 3));
-        System.out.println(intsCalc.sum(15, 12));
-        System.out.println(intsCalc.pow(5, 3));
+        System.out.println(intsCalc.sum(5, 5));
+        System.out.println(intsCalc.mult(5, 5));
+        System.out.println(intsCalc.pow(5, 5));
     }
 }
